e9b98e426eb25c5355dfeee260bf9c0995cceb40
Andre Felipe de Lara <andre.lara1@alunos.sc.senac.br>
2022-09-19 19:44:29 -0300

    criacao metodo composicao

---
+++ src/composicao/Motor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicao;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Motor {
+    
+    private int potencia;
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+    
+}
